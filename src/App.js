5c2800d7384652826aa32cf3d5503391452ad00d
paramesh-75 <parameshwaranit22@gmail.com>
2024-07-17 22:16:39 +0530

    2.MyForm using controlled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CounterApp from './components/CounterApp'
+import MyForm from './components/MyForm'
 
 const App = () => {
   return (
     <div>
       <CounterApp/>
+      <MyForm/>
     </div>
   )
 }
---
+++ src/components/MyForm.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const MyForm = () => {
+  const [input, setInput] = useState({})
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log(input);
+    }
+    const handleChange = (e) =>{
+      const {name, value} = e.target
+      setInput((prev)=>{return {...prev, [name]: value}})
+    }
+  return (
+    <div>
+        {/* using contro component */}
+        <form onSubmit={handleClick}>
+            <label><input type="text" onChange={handleChange} value={input.name} name='name'/>Name</label>
+            <label><input type="text" onChange={handleChange} value={input.email} name='email'/>Email</label>
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default MyForm;
