7c2467bdff7d036f3d7464b201a9be0906c8a16c
paramesh-75 <parameshwaranit22@gmail.com>
2024-07-18 22:09:45 +0530

    4.FetchData learned

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CounterApp from './components/CounterApp'
 import MyForm from './components/MyForm'
+import FetchData from './components/FetchData'
 
 const App = () => {
   return (
     <div>
       <CounterApp/>
       <MyForm/>
+      <FetchData/>
     </div>
   )
 }
---
+++ src/components/FetchData.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const FetchData = () => {
+    const [users, setUsers] = useState([])
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res)=>res.json())
+        .then((data)=>setUsers(data))
+        .catch((err)=>console.log(err))
+    },[])
+  return (
+    <div>
+        <table border='1'>
+            <thead>
+               <tr>
+               <td>Id</td>
+                <td>Name</td>
+                <td>Email</td>
+                </tr>
+               
+            </thead>
+            <tbody>
+                { users.map((user)=>(
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.emai}</td>
+                </tr>
+                ))
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default FetchData
