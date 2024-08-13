30382532fe50d83196511ed7aaa1f07dd9901558
paramesh-75 <parameshwaranit22@gmail.com>
2024-08-13 12:57:28 +0530

    username created

--- src/components/Object.jsx
+++ src/components/Object.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Object = () => {
+  const Person = {
+    name: 'John',
+    age: 30,
+    gender: 'male'
+  }
   return (
     <div>
         <h1>Object</h1>
-        
+        <p>Object is a collection of key-value pairs.</p>
+        <label htmlFor="">UserName<input type="text" /></label>
     </div>
   )
 }
